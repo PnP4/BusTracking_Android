37e2c9760e51ef801cb6f36bb85ad3ec1dc0ce10
DLPDS <tcdlpds@gmail.com>
2016-09-18 21:05:06 +0530

    Add google data marker with alert with network

--- app/src/main/java/com/ucsc/pnp/bustrack/MainActivity.java
+++ app/src/main/java/com/ucsc/pnp/bustrack/MainActivity.java
@@ -5,17 +5,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,14 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
     private GoogleMap googlemap;
     private Marker busmarker;
-
+    AlertReceive myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter("com.ucsc.pnp.bustrack.CUSTOM");
+
+        myReceiver = new AlertReceive();
+        registerReceiver(myReceiver, filter);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        googlemap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +59,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        startService(new Intent(this, NetService.class));
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(myReceiver);
     }
 
     @Override
@@ -73,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            String msg=intent.getStringExtra("Alert");
+            String msg=intent.getStringExtra("data");
+            Log.e("Service", msg);
             try {
                 JSONObject obj = new JSONObject(msg);
                 JSONArray objarr=obj.getJSONArray("alert");
@@ -88,12 +112,15 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject tempobjpos=tempobj.getJSONObject("movedata");
 
                             LatLng pos=new LatLng(tempobjpos.getDouble("lat"),tempobjpos.getDouble("lon"));
+
                             showDialog("Over waiting :- " + tempobj.getString("regno")+" ");
                             if(busmarker==null) {
                                 busmarker = googlemap.addMarker(new MarkerOptions().position(pos));
+
                             }
                             else{
                                 busmarker.setPosition(pos);
+
                             }
 
                         }
